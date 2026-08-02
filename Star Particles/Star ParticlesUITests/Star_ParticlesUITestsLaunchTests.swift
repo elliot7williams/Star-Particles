f19bcbf2699819e1aa57aa6f3221aff6499041c0
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:17:05 -0400

    Add Star Particles/Star ParticlesUITests/Star_ParticlesUITestsLaunchTests.swift

---
+++ Star Particles/Star ParticlesUITests/Star_ParticlesUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Star_ParticlesUITestsLaunchTests.swift
+//  Star ParticlesUITests
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import XCTest
+
+final class Star_ParticlesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
