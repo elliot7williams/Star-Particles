4bcdb534e8eb88247147d9c3b14b809743330d91
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:16:57 -0400

    Add Star Particles/Star ParticlesTests/Star_ParticlesTests.swift

---
+++ Star Particles/Star ParticlesTests/Star_ParticlesTests.swift
@@ -0,0 +1,17 @@
+//
+//  Star_ParticlesTests.swift
+//  Star ParticlesTests
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import Testing
+@testable import Star_Particles
+
+struct Star_ParticlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
